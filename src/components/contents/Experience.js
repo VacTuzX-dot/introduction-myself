5691e7e9cd99d15f573e3743f5b74f9992df6904
tydevilish <meawpain12@gmail.com>
2025-02-12 18:11:06 +0700

    update picture experience path

--- src/components/contents/Experience.js
+++ src/components/contents/Experience.js
@@ -1,6 +1,6 @@
 export const data1 = {
     date: "Aug 2023",
-    image: "https://scontent.fcnx1-1.fna.fbcdn.net/v/t39.30808-6/305296934_543056927825510_1540154849634592584_n.png?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFXNHo0rOWOAA7K10Rx8xwaFMS5q5HeeAIUxLmrkd54AlNlwzvBPstQ77roXZbxDReRPKVrvlKummQ-ucDJ2dpU&_nc_ohc=cq8MgeJHWRwQ7kNvgF1SoI7&_nc_oc=AdiLeRG0h364yi0G3RUi4CjXMh7GgpZxVIy8HWyzlrC70mxh0Xb-XLmfUt-qVIGx09w&_nc_zt=23&_nc_ht=scontent.fcnx1-1.fna&_nc_gid=A4kh-ueLXr2PZjMIQXOi1zT&oh=00_AYD6qYNN5jIl2OA1kIDmqSyYAqyERB6fUXRg_xCskPo5XA&oe=67ABB992",
+    image: "https://i.imgur.com/f2VZZf6.png",
     title: "Chiang Mai Software and Hardware Service Lo.,Ltd.",
     link1: "https://devcm.info",
     link2: "https://www.facebook.com/profile.php?id=100063636215596",
